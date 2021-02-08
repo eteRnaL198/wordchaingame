7113f3dbe8ad18feb9685ce43eefdb5bb12f38ff
eternal198 <ikuya313198@gmail.com>
2021-02-09 00:01:39 +0900

    2/8

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,28 @@ import './assets/styles/App.css';
 import { Chat, Input } from "./components/index";
 import jsonData from "./data.json";
 import firebase from "firebase";
+import writeData from "./writeData";
+
+// writeData();
 
 type Message = {
   text: string;
   whose: string;
 }
 
-(() => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCOIXzz4vmffj94FLMWhEX0mE4t0UMTsxc",
-    authDomain: "wordchaingame-3e0fc.firebaseapp.com",
-    projectId: "wordchaingame-3e0fc",
-    storageBucket: "wordchaingame-3e0fc.appspot.com",
-    messagingSenderId: "307489909046",
-    appId: "1:307489909046:web:4bb2441c4c44a671406b97"
-  };
-  const app = firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore(app);
-  const docRef = db.collection("words").doc("a_line");
-  docRef.get().then((doc) => {
-    console.log("Document data:", doc.data());
-  })
-})();
+// (async () => {
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyCOIXzz4vmffj94FLMWhEX0mE4t0UMTsxc",
+//     authDomain: "wordchaingame-3e0fc.firebaseapp.com",
+//     projectId: "wordchaingame-3e0fc",
+//     storageBucket: "wordchaingame-3e0fc.appspot.com",
+//     messagingSenderId: "307489909046",
+//     appId: "1:307489909046:web:4bb2441c4c44a671406b97"
+//   };
+//   const app = firebase.initializeApp(firebaseConfig);
+//   const db = firebase.firestore(app);
+//   const docRef = db.collection("words").doc("s_line");
+// })();
 
 function App() {
   const [messages, setMessages] = useState(jsonData.messages);
@@ -50,5 +50,3 @@ function App() {
 }
 
 export default App;
-
-// TODO  https://xn--68j8a5fb.net/ 単語写す
