40d99a97cb9a6109b97e91c269711b98cbf3f2e0
eternal198 <ikuya313198@gmail.com>
2021-02-10 15:27:41 +0900

    2/10

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,29 @@ type Message = {
   whose: string;
 }
 
+type FetchWord = (char: string) => Promise<string | null>;
+
+const scrollBottom = () => {
+  const chatsWrapper = document.getElementById("js_chats_wrapper");
+  if(chatsWrapper) {
+    const lastBalloon = chatsWrapper.lastElementChild;
+    if(lastBalloon) lastBalloon.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+}
+
 function App() {
-  const [messages, setMessages] = useState(jsonData.messages);
+  const [messages, setMessages] = useState<Message[]>(jsonData.messages);
 
   useEffect(() => {
     console.log(messages);
+    return () => {
+      scrollBottom();
+    }
     // 負け判定
   }, [messages]);
 
-  const fetchWord = (char: string) => {
-    if(firebase.app.length) {
+  const updateMessage = async (newMyMessage: Message) => {
+    if(!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: "AIzaSyCOIXzz4vmirffj94FLMWhEX0mE4t0UMTsxc",
         authDomain: "wordchaingame-3e0fc.firebaseapp.com",
@@ -31,33 +44,40 @@ function App() {
         appId: "1:307489909046:web:4bb2441c4c44a671406b97"
       });
     }
-    (async () => {
-      const db = firebase.firestore();
-      const doc = await db.collection("words").doc(char).get();
-      const field = doc.get("a")
-      const idx = Math.floor(Math.random() * field.length)
-    })
-    return field[idx].word;
-  }  
+    const db = firebase.firestore();
+    const lastChar = newMyMessage.word.slice(-1);
+    console.log(lastChar);
+    const doc = await db.collection("words").doc(lastChar).get()
+      .then((res) => res)
+      .catch((err) => {
+        console.error(err);
+        return null;
+      })
+    if(!doc) return null;
+    // const doc = await db.collection("words").doc(char).get();
+    const field = await doc.get("a")
+    const idx = Math.floor(Math.random() * field.length)
+    const newOthersMessage: Message = {
+          word: `${field[idx].word}  ( ${field[idx].desc} )`,
+          whose: "others"
+    }
+    setMessages([...messages, newMyMessage, newOthersMessage]);
+    return null;
+    // return field[idx].word;
+  }
+   
 
   const handleWordAdd = (newWord: string) => {
-    const newMessage: Message = {
+    const newMyMessage: Message = {
       word: newWord,
       whose: "mine"
     }
-    const lastChar = newWord.slice(-1);
-    setMessages([...messages, newMessage]);
-    const opponentWord = fetchWord(lastChar);
-    const newOpponentMessage: Message = {
-      word: opponentWord,
-      whose: "others"
-    }
-    setMessages([...messages, opponentWord]);
+    updateMessage(newMyMessage);
   }
 
   return (
     <div className="main">
-      <div className="chats_wrapper">
+      <div className="chats_wrapper" id="js_chats_wrapper">
         {messages.map((message, idx) => (
           <Chat key={idx} message={message} />
         ))}
